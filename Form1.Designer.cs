758598aeeb9ed295e3b3b3a9b5f3aa924995bdc5
nekoolya <36623096+nekoolya@users.noreply.github.com>
2018-03-28 13:21:02 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace DataAirBnB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend9 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea10 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend10 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea11 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend11 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea12 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend12 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart6 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart7 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart8 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart3
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart3.Legends.Add(legend7);
+            this.chart3.Location = new System.Drawing.Point(-23, -46);
+            this.chart3.Name = "chart3";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart3.Series.Add(series7);
+            this.chart3.Size = new System.Drawing.Size(300, 300);
+            this.chart3.TabIndex = 0;
+            this.chart3.Text = "chart1";
+            // 
+            // chart4
+            // 
+            chartArea8.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chart4.Legends.Add(legend8);
+            this.chart4.Location = new System.Drawing.Point(308, 6);
+            this.chart4.Name = "chart4";
+            series8.ChartArea = "ChartArea1";
+            series8.Legend = "Legend1";
+            series8.Name = "Series1";
+            this.chart4.Series.Add(series8);
+            this.chart4.Size = new System.Drawing.Size(208, 248);
+            this.chart4.TabIndex = 1;
+            this.chart4.Text = "chart4";
+            // 
+            // chart5
+            // 
+            chartArea9.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea9);
+            legend9.Name = "Legend1";
+            this.chart5.Legends.Add(legend9);
+            this.chart5.Location = new System.Drawing.Point(522, 12);
+            this.chart5.Name = "chart5";
+            series9.ChartArea = "ChartArea1";
+            series9.Legend = "Legend1";
+            series9.Name = "Series1";
+            this.chart5.Series.Add(series9);
+            this.chart5.Size = new System.Drawing.Size(210, 243);
+            this.chart5.TabIndex = 2;
+            this.chart5.Text = "chart5";
+            // 
+            // chart6
+            // 
+            chartArea10.Name = "ChartArea1";
+            this.chart6.ChartAreas.Add(chartArea10);
+            legend10.Name = "Legend1";
+            this.chart6.Legends.Add(legend10);
+            this.chart6.Location = new System.Drawing.Point(12, 260);
+            this.chart6.Name = "chart6";
+            series10.ChartArea = "ChartArea1";
+            series10.Legend = "Legend1";
+            series10.Name = "Series1";
+            this.chart6.Series.Add(series10);
+            this.chart6.Size = new System.Drawing.Size(265, 300);
+            this.chart6.TabIndex = 3;
+            this.chart6.Text = "chart6";
+            // 
+            // chart7
+            // 
+            chartArea11.Name = "ChartArea1";
+            this.chart7.ChartAreas.Add(chartArea11);
+            legend11.Name = "Legend1";
+            this.chart7.Legends.Add(legend11);
+            this.chart7.Location = new System.Drawing.Point(308, 260);
+            this.chart7.Name = "chart7";
+            series11.ChartArea = "ChartArea1";
+            series11.Legend = "Legend1";
+            series11.Name = "Series1";
+            this.chart7.Series.Add(series11);
+            this.chart7.Size = new System.Drawing.Size(209, 300);
+            this.chart7.TabIndex = 4;
+            this.chart7.Text = "chart2";
+            // 
+            // chart8
+            // 
+            chartArea12.Name = "ChartArea1";
+            this.chart8.ChartAreas.Add(chartArea12);
+            legend12.Name = "Legend1";
+            this.chart8.Legends.Add(legend12);
+            this.chart8.Location = new System.Drawing.Point(534, 261);
+            this.chart8.Name = "chart8";
+            series12.ChartArea = "ChartArea1";
+            series12.Legend = "Legend1";
+            series12.Name = "Series1";
+            this.chart8.Series.Add(series12);
+            this.chart8.Size = new System.Drawing.Size(198, 300);
+            this.chart8.TabIndex = 5;
+            this.chart8.Text = "chart3";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1056, 512);
+            this.Controls.Add(this.chart8);
+            this.Controls.Add(this.chart7);
+            this.Controls.Add(this.chart6);
+            this.Controls.Add(this.chart5);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart8)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace DataAirBnB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+  
+
+            City[] stader = new City[] // listan med städer
+            {
+                new City("Amsterdam2",0,0,0,0,0), //skapar city objekt
+                new City("Barcelona2",0,0,0,0,0),
+                new City("Boston2",0,0,0,0,0),
+                };
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=LAPTOP-OBD0K6JL;Initial Catalog=airbnbtest;Integrated Security=True";
+
+            for (int i = 0; i < stader.Length; i++) // hämtar 3 tabbeler från SQL
+            {
+
+                    conn.Open();
+
+                    SqlCommand myQuery = new SqlCommand("SELECT * FROM " + stader[i].Namn, conn); //same name as in Boston2 i SQL
+                    SqlDataReader myReader = myQuery.ExecuteReader();
+
+                    while (myReader.Read()) // sätter in rätt objekt
+
+                    {
+
+                        int host_id = (int)myReader["host_id"];  // Reading all variables from database.
+                        string room_type = (string)myReader["room_type"].ToString();
+                        int room_id = (int)myReader["room_id"];
+                        string borough; //= (string)myReader["borough"];
+
+                        if (myReader["borough"] is String)
+                        {
+
+                            borough = (string)myReader["borough"];
+                        }
+
+                        else
+
+                        {
+
+                            borough = "";
+                        }
+
+                        string neighborhood = (string)myReader["neighborhood"];
+                        int reviews = (int)myReader["Reviews"];
+
+                        string var1 = (string)myReader["Overall_satisfaction"];
+                        double Overall_satisfaction;
+                        double.TryParse(var1, out Overall_satisfaction); //Overall_satisfaction får värdet som skickas ut i out
+                        int accomodates = (int)myReader["accommodates"];
+
+                        var1 = (string)myReader["Bedrooms"];
+                        double d_bedrooms;
+                        double.TryParse(var1, out d_bedrooms);
+                        int bedrooms = (int)Math.Round(d_bedrooms);
+
+                        var1 = (string)myReader["Price"];
+                        double d_price;
+                        double.TryParse(var1, out d_price);
+                        int price = (int)Math.Round(d_price);
+
+                        var1 = myReader["minstay"].ToString();
+                        double m_minstay;
+                        double.TryParse(var1, out m_minstay);
+                        int minstay = (int)Math.Round(m_minstay);
+
+                        var1 = (string)myReader["latitude"];
+                        double l_latitude;
+                        double.TryParse(var1, out l_latitude);
+                        int latitude = (int)Math.Round(l_latitude);
+
+                        var1 = (string)myReader["Longitude"];
+                        double l_longitude;
+                        double.TryParse(var1, out l_longitude);
+                        int longitude = (int)Math.Round(l_longitude);
+
+                        string last_modified = (string)myReader["last_modified"].ToString();
+
+                        Accommondation getAccommondation = new Accommondation(room_id, host_id, room_type, borough, neighborhood, reviews, Overall_satisfaction,
+                        accomodates, bedrooms, price, minstay, latitude, longitude, last_modified);
+                        stader[i].addAccomm(getAccommondation);
+
+                    }
+                    conn.Close(); // hämtat och sparad, behöver inte ha det öppet
+
+            }
+
+            chart7.Titles.Add("Barcelona price");
+            chart7.ChartAreas[0].AxisX.Title = "Room";
+            chart7.ChartAreas[0].AxisY.Title = "Price";
+
+            chart8.Titles.Add("Amsterdam price");
+            chart8.ChartAreas[0].AxisX.Title = "Room";
+            chart8.ChartAreas[0].AxisY.Title = "Price";
+
+            chart3.Titles.Add("Boston price");
+            chart3.ChartAreas[0].AxisX.Title = "Room";
+            chart3.ChartAreas[0].AxisY.Title = "Price";
+
+            chart4.Titles.Add("Barcelona price/overall satisfaction");
+            chart4.ChartAreas[0].AxisX.Title = "Price";
+            chart4.ChartAreas[0].AxisY.Title = "Overall Satisfation";
+
+            chart5.Titles.Add("Amsterdam price/overall satisfaction");
+            chart5.ChartAreas[0].AxisX.Title = "Price";
+            chart5.ChartAreas[0].AxisY.Title = "Overall Satisfation";
+
+            chart6.Titles.Add("Boston price/overall satisfaction");
+            chart6.ChartAreas[0].AxisX.Title = "Price";
+            chart6.ChartAreas[0].AxisY.Title = "Overall Satisfation";
+
+
+            //Histogram 1,2 och 3
+            foreach (Accommondation a in stader[0].AccomList.Where(x => x.Room_type1 == "Private room")) //hämta data från objekt till graf
+            {
+                chart7.Series["Series1"].Points.AddY(a.Price1); // lägger till varje datapoint
+            }
+
+
+            chart7.Series["Series1"].ChartType = SeriesChartType.Column;
+
+
+            foreach (Accommondation a in stader[1].AccomList.Where (x => x.Room_type1 == "Private room"))
+            {
+                chart8.Series["Series1"].Points.AddY(a.Price1);
+            }
+
+            chart8.Series["Series1"].ChartType = SeriesChartType.Column;
+
+            foreach (Accommondation a in stader[2].AccomList.Where(x => x.Room_type1 == "Private room"))
+            {
+                chart3.Series["Series1"].Points.AddY(a.Price1);
+            }
+
+            chart3.Series["Series1"].ChartType = SeriesChartType.Column;
+
+            //Scatterplott 1,2 och 3 
+            foreach (Accommondation a in stader[0].AccomList.Where(x => x.Overall_satisfaction1 != 0 && x.Overall_satisfaction1 < 4.5))
+            {
+                chart4.Series["Series1"].Points.AddXY(a.Price1, a.Overall_satisfaction1);
+            }
+
+            chart4.Series["Series1"].ChartType = SeriesChartType.Point;
+
+            foreach (Accommondation a in stader[1].AccomList.Where(x => x.Overall_satisfaction1 != 0 && x.Overall_satisfaction1 < 4.5))
+            {
+                chart5.Series["Series1"].Points.AddXY(a.Price1, a.Overall_satisfaction1);
+            }
+
+            chart5.Series["Series1"].ChartType = SeriesChartType.Point;
+
+            foreach (Accommondation a in stader[2].AccomList.Where (x => x.Overall_satisfaction1 != 0 && x.Overall_satisfaction1 < 4.5))
+            {
+                chart6.Series["Series1"].Points.AddXY(a.Price1, a.Overall_satisfaction1);
+            }
+
+            chart6.Series["Series1"].ChartType = SeriesChartType.Point;
+
+
+
+        }
+    }
+
+      
+}
